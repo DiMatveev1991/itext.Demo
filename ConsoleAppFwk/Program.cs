38d8fbcefa10a15569c59f4868a82c50c491f850
saurabh dasgupta <saurabh_dasgupta@hotmail.com>
2019-08-30 16:09:09 +0100

    Added Console EXE

---
+++ ConsoleAppFwk/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFwk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string file = "CS15.page6.pdf";
+                byte[] contents = System.IO.File.ReadAllBytes(file);
+                ///
+                /// Invoke itext7
+                ///
+                var extractorPdfium = new SampleLib.Pdfium.NetSDK.Class1();
+                var pagesV7 = extractorPdfium.GetBlocks(contents);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
